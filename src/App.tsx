0f236e051e58877d5b999151d2d432ceb95a031c
Bhanuka <bhanukalm@gmail.com>
2020-09-07 22:22:40 +0530

    added header for testing dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import TimerModule from './modules/timer-module';
+import styled from 'styled-components';
+
+const Header = styled.h1`
+  color: #444;
+  text-align: center;
+  font-size: 2em;
+`
 
 function App() {
   return (
     <>
+      <Header>Test Dev</Header>
       <TimerModule />
     </>
   );
